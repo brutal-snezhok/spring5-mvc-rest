5a4340a12e092d2d2b8a2bef3f8f82aefa78c1a0
brutal-snezhok <czyrulik97@mail.ru>
2020-02-02 20:57:18 +0300

    Added requestBody annotation.

--- src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -6,10 +6,7 @@ import guru.springfamework.services.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/v1/customers")
@@ -35,7 +32,7 @@ public class CustomerController {
     }
 
     @PostMapping
-    public ResponseEntity<CustomerDTO> createNewCustomer(CustomerDTO customerDTO){
+    public ResponseEntity<CustomerDTO> createNewCustomer(@RequestBody CustomerDTO customerDTO){
         return new ResponseEntity<>(customerService.createNewCustomer(customerDTO), HttpStatus.CREATED);
     }
 }
